a85de115013f61ccc4e690de72810f72e127eaa9
Azwar Anas <azwarans23@gmail.com>
2024-09-12 21:22:23 +0800

    secondary commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Function Aritmatika
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero");
+  }
+  return a / b;
+}
+function modulo(a, b) {
+  return a % b;
+}
+
+// Check REST API
+function getAPI(url, callback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error("Error fetching data: " + error));
+}
+
+// Check Function
+function isNameValid(name) {
+  return /^[a-zA-Z]+$/.test(name);
+}
